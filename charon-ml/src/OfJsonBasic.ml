602ed7518b08f31411e60dfdb5f2412586b83d5b
N1ark <opale.sjostedt@gmail.com>
2026-04-17 20:03:40 +0200

    Better JSON error reporting

--- charon-ml/src/OfJsonBasic.ml
+++ charon-ml/src/OfJsonBasic.ml
@@ -9,33 +9,39 @@ let ( let* ) o f =
   | Error e -> Error e
   | Ok x -> f x
 
+let pp_js = Yojson.Basic.pretty_print ~std:true
+
 let combine_error_msgs (js : json) (msg : string) (res : ('a, string) result) :
     ('a, string) result =
   match res with
   | Ok x -> Ok x
-  | Error e -> Error ("[" ^ msg ^ "]" ^ " failed on: " ^ show js ^ "\n\n" ^ e)
+  | Error e ->
+      Error (Format.asprintf "[%s] failed on:@.%a\n\n%s" msg pp_js js e)
 
 let bool_of_json (ctx : 'ctx) (js : json) : (bool, string) result =
   match js with
   | `Bool b -> Ok b
-  | _ -> Error ("bool_of_json: not a bool: " ^ show js)
+  | _ -> Error (Format.asprintf "bool_of_json: not a bool: %a" pp_js js)
 
 let int_of_json (ctx : 'ctx) (js : json) : (int, string) result =
   match js with
   | `Int i -> Ok i
-  | _ -> Error ("int_of_json: not an int: " ^ show js)
+  | _ -> Error (Format.asprintf "int_of_json: not an int: %a" pp_js js)
 
 let char_of_json (ctx : 'ctx) (js : json) : (Uchar.t, string) result =
   match js with
   | `String c ->
       if String.length c > 4 then
-        Error ("char_of_json: stricly more than four bytes in: " ^ show js)
+        Error
+          (Format.asprintf "char_of_json: more than four chars: %a" pp_js js)
       else
         let uchar = String.get_utf_8_uchar c 0 in
         if Uchar.utf_decode_is_valid uchar then
           Ok (Uchar.utf_decode_uchar uchar)
-        else Error ("char_of_json: invalid UTF-8 character: " ^ show js)
-  | _ -> Error ("char_of_json: not a char: " ^ show js)
+        else
+          Error
+            (Format.asprintf "char_of_json: invalid UTF-8 char: %a" pp_js js)
+  | _ -> Error (Format.asprintf "char_of_json: not a string: %a" pp_js js)
 
 let rec of_json_list (a_of_json : 'ctx -> json -> ('a, string) result)
     (ctx : 'ctx) (jsl : json list) : ('a list, string) result =
@@ -54,7 +60,7 @@ let pair_of_json (a_of_json : 'ctx -> json -> ('a, string) result)
       let* a = a_of_json ctx a in
       let* b = b_of_json ctx b in
       Ok (a, b)
-  | _ -> Error ("pair_of_json failed on: " ^ show js)
+  | _ -> Error (Format.asprintf "pair_of_json failed on: %a" pp_js js)
 
 let triple_of_json (a_of_json : 'ctx -> json -> ('a, string) result)
     (b_of_json : 'ctx -> json -> ('b, string) result)
@@ -66,19 +72,19 @@ let triple_of_json (a_of_json : 'ctx -> json -> ('a, string) result)
       let* b = b_of_json ctx b in
       let* c = c_of_json ctx c in
       Ok (a, b, c)
-  | _ -> Error ("triple_of_json failed on: " ^ show js)
+  | _ -> Error (Format.asprintf "triple_of_json failed on: %a" pp_js js)
 
 let list_of_json (a_of_json : 'ctx -> json -> ('a, string) result) (ctx : 'ctx)
     (js : json) : ('a list, string) result =
   combine_error_msgs js "list_of_json"
     (match js with
     | `List jsl -> of_json_list a_of_json ctx jsl
-    | _ -> Error ("not a list: " ^ show js))
+    | _ -> Error (Format.asprintf "list_of_json: not a list: %a" pp_js js))
 
 let string_of_json (ctx : 'ctx) (js : json) : (string, string) result =
   match js with
   | `String str -> Ok str
-  | _ -> Error ("string_of_json: not a string: " ^ show js)
+  | _ -> Error (Format.asprintf "string_of_json: not a string: %a" pp_js js)
 
 let option_of_json (a_of_json : 'ctx -> json -> ('a, string) result)
     (ctx : 'ctx) (js : json) : ('a option, string) result =
@@ -105,4 +111,4 @@ let key_value_pair_of_json (a_of_json : 'ctx -> json -> ('a, string) result)
       let* a = a_of_json ctx a in
       let* b = b_of_json ctx b in
       Ok (a, b)
-  | _ -> Error ("key_value_pair_of_json failed on: " ^ show js)
+  | _ -> Error (Format.asprintf "key_value_pair_of_json failed on: %a" pp_js js)
